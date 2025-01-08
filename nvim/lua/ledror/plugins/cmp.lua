28335ee25c0cde88ae40fd5ecbe7989f76798c51
Dror Elmakias <drorelmakias@gmail.com>
2025-01-08 22:08:51 +0200

    Holy shit I'm dumb

--- nvim/lua/ledror/plugins/cmp.lua
+++ nvim/lua/ledror/plugins/cmp.lua
@@ -24,8 +24,8 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["C-n"] = cmp.mapping.select_next_item(),
-				["C-p"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
 				["<C-u>"] = cmp.mapping.scroll_docs(4),
 				["<C-e>"] = cmp.mapping.close(),
