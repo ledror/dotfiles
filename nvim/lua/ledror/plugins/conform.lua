b488b273843c24df8ec2180423411cdd9806460d
Dror Elmakias <drorelmakias@gmail.com>
2025-01-08 21:55:12 +0200

    Add trim_whitespace and format-on-save

--- nvim/lua/ledror/plugins/conform.lua
+++ nvim/lua/ledror/plugins/conform.lua
@@ -14,6 +14,12 @@ return {
 		formatters_by_ft = {
 			c = { "clang-format" },
 			cpp = { "clang-format" },
+			["_"] = { "trim_whitespace" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
 		},
 	},
 }
