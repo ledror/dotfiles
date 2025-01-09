cdcd9c03f2881276537ddd38bbdbc4661fb2f519
Dror Elmakias <drorelmakias@gmail.com>
2025-01-09 21:23:06 +0200

    Fix scroll direction

--- nvim/lua/ledror/plugins/cmp.lua
+++ nvim/lua/ledror/plugins/cmp.lua
@@ -26,8 +26,8 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-n>"] = cmp.mapping.select_next_item(),
 				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-u>"] = cmp.mapping.scroll_docs(4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
 				["<C-e>"] = cmp.mapping.close(),
 				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Insert,
