ac6ab67e4fe18947b28993597b0cf96189489cbb
Dror Elmakias <drorelmakias@gmail.com>
2025-01-09 21:23:29 +0200

    Disable lazy loading of nvim-tree
    It fucks up opening directories with "nvim <dir>", since it's not loaded
    yet

--- nvim/lua/ledror/plugins/nvim-tree.lua
+++ nvim/lua/ledror/plugins/nvim-tree.lua
@@ -1,10 +1,9 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "<leader>e", "<cmd>NvimTreeToggle<cr>" },
-	},
 	config = function()
 		require("nvim-tree").setup()
+		local api = require("nvim-tree.api")
+		vim.keymap.set("n", "<leader>e", api.tree.toggle)
 	end,
 }
