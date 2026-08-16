0bdc8b5398e645388d5848553f36fd09affb81b1
Dror Elmakias <drorelmakias@gmail.com>
2026-08-17 02:06:31 +0300

    Whoops

--- nvim/lua/ledror/plugins/snacks.lua
+++ nvim/lua/ledror/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
 		input = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
-		scroll = { enabled = true },
+		scroll = { enabled = false },
 		statuscolumn = { enabled = true },
 		words = { enabled = false },
 		notifier = {
